06956ff648df34fa8e9aef495b84406e6dd0c2e2
Shivani Kumari <95735713+shivani889@users.noreply.github.com>
2026-08-21 15:00:40 +0530

    Update JobPage.jsx

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -112,11 +112,16 @@ const JobPage = ({ deleteJob }) => {
 };
 
 const jobLoader = async ({ params }) => {
-  const res = await fetchhttps://job-listings-api-z99n.onrender.com/jobs/${params.id}`);
+  const res = await fetch(
+    `https://job-listings-api-z99n.onrender.com/jobs/${params.id}`
+  );
+
   if (!res.ok) {
     throw new Error(`Failed to fetch job: ${res.status}`);
   }
+
   const data = await res.json();
+
   return data;
 };
 export { JobPage as default, jobLoader };
